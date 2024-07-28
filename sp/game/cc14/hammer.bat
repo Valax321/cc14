a5971c16a88e09dee68932cc50425a7ef3c4d4b6
Audrey Castillo <valax321@gmail.com>
2024-07-28 16:21:30 +1000

    Add test map

---
+++ sp/game/cc14/hammer.bat
@@ -0,0 +1,4 @@
+@echo off
+set GAMEBIN="%STEAMDIR%\steamapps\common\Source SDK Base 2013 Singleplayer\bin\hammerplusplus.exe"
+echo Locating Source SDK at %GAMEBIN%
+%GAMEBIN%
--- sp/game/cc14/launch.bat
+++ sp/game/cc14/launch.bat
@@ -1,4 +1,4 @@
 @echo off
 set GAMEBIN="%STEAMDIR%\steamapps\common\Source SDK Base 2013 Singleplayer\hl2.exe"
 echo Locating Source SDK at %GAMEBIN%
-%GAMEBIN% -game %~dp0 -windowed -noborder -width 1920 -height 1080 -nop4 +developer 2 +sv_cheats 1
+%GAMEBIN% -game %~dp0 -windowed -width 1600 -height 900 -nop4 +developer 2 +sv_cheats 1
